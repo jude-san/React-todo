d4ed29889105fad4414bdfb4724063c52222aaf5
jude-san <judekhay@gmail.com>
2023-06-14 00:14:48 +0000

    reset

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const subject = "React"; 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello {subject}
         </p>
       </header>
     </div>
