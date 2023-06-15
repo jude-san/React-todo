3a60786463614d8e66fe2b67783d795d275efcda
jude-san <judekhay@gmail.com>
2023-06-15 00:24:27 +0000

    none

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const subject = "React"; 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {subject}
-        </p>
-      </header>
-    </div>
-  );
+function App(props) {
+  console.log(props); 
+  return(props);
 }
 
 export default App;
