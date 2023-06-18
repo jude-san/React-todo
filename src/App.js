668ffe53219a7a9a54b3c758455a1c2cc9f30410
jude-san <judekhay@gmail.com>
2023-06-18 20:24:07 +0000

    mind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
 
   console.log(props);
-  // const subject = props.subject;
+  const subject = props.subject;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello, {subject}!  Welcome Back!</p>
+      </header>
+    </div>
+  );
   
 }
 
